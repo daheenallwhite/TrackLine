36da26ce657c0091357a1944693a5401031883b2
daheenallwhite <allwhite.dev@gmail.com>
2024-09-08 22:11:28 +0900

    Create new project

---
+++ TrackLine/TrackLine/TrackLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackLineApp.swift
+//  TrackLine
+//
+//  Created by 흰 on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
